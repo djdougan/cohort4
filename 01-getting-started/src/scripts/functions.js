8029901aab1eaf8ab8518e311c73fa5e87d5871c
Douglas Dougan <djdougan@ddougan.com>
2020-02-21 22:25:32 -0700

    comp 100b added extra large functionality

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -1,10 +1,11 @@
 
 const functions = {
-    
+
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num < 101) return "large";
+        return "extra large";
     },
 
     add: (num1, num2) => {
@@ -15,7 +16,7 @@ const functions = {
         return num1 - num2;
     }
 
-    
+
 };
 
 export default functions;
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -6,10 +6,11 @@ test('Check the sizes', () => {
     expect(functions.size(10)).toBe("medium");
     expect(functions.size(15)).toBe("medium");
     expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+    expect(functions.size(101)).toBe("extra large");
+    expect(functions.size(2000000)).toBe("extra large");
 });
 
 test('Does that add function work?', () => {
-    expect(functions.add(1,2)).toBe(3);
-    expect(functions.add(101,202)).toBe(303);
+    expect(functions.add(1, 2)).toBe(3);
+    expect(functions.add(101, 202)).toBe(303);
 });
