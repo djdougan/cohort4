2a5ea001d996afd4e03f780e799d3f0f1412ef12
Douglas Dougan <djdougan@ddougan.com>
2020-02-24 22:57:37 -0700

    define attributes / variables section 100C

---
+++ 01-getting-started/src/scripts/syntax.js
@@ -0,0 +1,103 @@
+// •	define attributes / variables
+//        (X) o	number
+//        (X) o	string
+//        (X) o	boolean
+//        (X) o	array
+//        (X) o	dictionary / objects
+//        (X) o	undefined
+// •	sample if / else
+// •	functions
+//          o	parameters
+//          o	returns
+// •	arrays
+//          o	add to the front
+//          o	add to the end
+//          o	update values
+// •	loops
+//          o	for
+//          o	for/in
+//          o	while
+//          o	do while
+//          o	forEach(with array and function)
+// •	Objects / Dictionaries
+//          o	declare object
+//          o	lookup key to retrieve value
+
+/**
+ * <summary></summary>
+ */
+const syntax = {
+    /**
+     * @description Returns the result of a number raised to a power.
+     * @name power
+    * @param {number} number -- Required.The base number.It can be any real number.
+    * @param {number} power -- Required.The exponent to which the base number is raised.
+    * @returns {number} The result of a number raised to a power.
+     */
+    power: (number, power) => {
+        return Math.pow(number, power);
+    },
+    /**
+     * @description Removes a char(s) from a word or sentence.
+     * @name removeChosenOne
+    * @param {string} str -- A word or sentence.
+    * @param {string} char -- character(s) to be removed from sentence.
+    * @returns {string} The word(s) with the character(s) removed.
+     */
+    removeChosenOne: (str, char) => {
+        let regExp = new RegExp(`([${char}])+`, 'gi');
+        return str.replace(regExp, '');
+    },
+
+    /**
+     * @description Checks if number is in a high low range.
+     * @name inRange
+    * @param {number} min -- A word or sentence.
+    * @param {number} max -- character(s) to be removed from sentence.
+    * @param {number} testNum -- character(s) to be removed from sentence.
+    * @returns {boolean} The word(s) with the character(s) removed.
+     */
+    inRange: (min, max, testNum) => {
+        return testNum >= min && testNum <= max;
+    },
+
+    /**
+     * @description checks to see if only number are in the array.
+     * @name onlyNumbersInArray
+    * @param {number[]} numbers -- an array of numbers.
+    * @returns {boolean} true is array has only number false if any other object found.
+     */
+    onlyNumbersInArray: (numberArray) => {
+        return !numberArray.some(isNaN);
+    },
+
+    /**
+     * @description Checks to see if person is a certain age or older.
+     * @name ageCheck
+    * @param {number} testAge -- age to test against.
+    * @param {Object.<string, number>} Person -- person object to test against.
+    * @returns {boolean} true if the person is over testAge, false if .
+     */
+    ageCheck: (testAge, personObject) => {
+        return personObject.age >= testAge;
+    },
+
+    /**
+     * @description Checks is the value has been assigned a value.
+     * @name isDefined
+    * @param {number|string|boolean} aType -- value to be tested for a value.
+    * @returns {boolean} tue if the type has a value .
+     */
+    isDefined: (aType) => {
+        return true;
+    }
+
+
+
+
+
+
+}
+
+
+export default syntax;
---
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -0,0 +1,59 @@
+
+
+/** 
+ * @author Douglas J Dougan <djdougan@gmail.com>
+ * @
+ * @description test file for syntax.js
+ * @example to run syntax tests use: npm test -- 'syntax.test.js'
+ * 
+ */
+import syntax from './syntax';
+test("Test the numbers", () => {
+    expect(syntax.power(2, 4)).toBe(16);
+    expect(syntax.power(2, 4)).not.toBe(9);
+    expect(syntax.power(2, 5)).toBe(32);
+    expect(syntax.power(2, 5)).not.toBe(40);
+});
+
+
+test("Test character removal", () => {
+    expect(syntax.removeChosenOne('Full stack web developer', 'ae')).toMatch('Full stck wb dvlopr');
+
+    expect(syntax.removeChosenOne('Supercalifragilisticexpialidocious', 'aeiouy')).toMatch('Sprclfrglstcxpldcs');
+    expect(syntax.removeChosenOne('smart water', 'a')).toMatch('smrt wter');
+    expect(syntax.removeChosenOne('super function', 'u')).toMatch('sper fnction');
+    expect(syntax.removeChosenOne('The quick brown fox jumps over the lazy dog', 'aeiouy')).toMatch('Th qck brwn fx jmps vr th lz dg');
+    // did not remove the y out of lazy
+    expect(syntax.removeChosenOne('The quick brown fox jumps over the lazy dog', 'aeiouy')).not.toMatch('Th qck brwn fx jmps vr th lzy dg');
+});
+
+test("Test if number is in range", () => {
+    expect(syntax.inRange(10, 20, 15)).toBe(true);
+    expect(syntax.inRange(10, 20, 21)).toBe(false);
+    expect(syntax.inRange(10, 12, 11)).toBe(true);
+    // min is larger than max
+    expect(syntax.inRange(20, 10, 15)).toBe(false);
+})
+
+test("Test if only numbers are in array", () => {
+    // all numbers returns true
+    expect(syntax.onlyNumbersInArray([1, 5, 15, 20, 50, 100])).toBe(true);
+    // contains a string return false
+    expect(syntax.onlyNumbersInArray([1, 2, 3, "dog"])).toBe(false);
+    expect(syntax.onlyNumbersInArray([111, 345, 315])).toBe(true);
+    // contains a object returns false
+    expect(syntax.onlyNumbersInArray([1, 2, 3, { x: 3, y: 5 }])).toBe(false);
+
+});
+
+test("Test persons age", () => {
+    expect(syntax.ageCheck(18, { name: "James", age: 18 })).toBe(true);
+    expect(syntax.ageCheck(21, { name: "Anne", age: 20 })).toBe(false);
+    expect(syntax.ageCheck(55, { name: "Dan", age: 56 })).toBe(true);
+    expect(syntax.ageCheck(65, { name: "Allen", age: 65 })).toBe(true);
+});
+
+test("Test if Object is undefined", () => {
+    let age;
+    expect(syntax.isDefined(age)).toBe(true);
+});
