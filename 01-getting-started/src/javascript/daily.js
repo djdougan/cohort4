fe6c19b81b182a65e816ccf03336b1cdae1a3ba9
Douglas Dougan <djdougan@ddougan.com>
2020-03-04 14:02:08 -0700

    daily.js and daily.test.js

---
+++ 01-getting-started/src/javascript/daily.js
@@ -0,0 +1,10 @@
+
+const TemperatureConvertor = {
+    convertToFahrenheit: (celsius) => {
+        let result = celsius * 9 / 5 + 32;
+
+        return result;
+    }
+};
+
+export default TemperatureConvertor;
