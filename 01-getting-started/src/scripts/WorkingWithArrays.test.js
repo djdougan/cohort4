232688462ba63a9f997c24f8e9166baf202a600b
Douglas Dougan <djdougan@ddougan.com>
2020-03-05 19:55:21 -0700

    change tests

---
+++ 01-getting-started/src/scripts/WorkingWithArrays.test.js
@@ -0,0 +1,10 @@
+import WorkingWithArrays from './WorkingWithArrays.js';
+
+
+test("Test: clear(array) returns []", () => {
+    expect(WorkingWithArrays.clear()).toEqual([]);
+    expect(WorkingWithArrays.clear()).toEqual([]);
+    expect(WorkingWithArrays.clear()).toEqual([]);
+});
+
+test("Test: clear(array) returns []")
