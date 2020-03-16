aa6cd29fe1b9b43e4e32b400e32b2b8f17379fa7
Douglas Dougan <djdougan@ddougan.com>
2020-03-16 15:30:05 -0600

    created Account.js/Account.test.js

---
+++ 03-objects/src/scripts/Account.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2020
+ *
+ * @summary Working with objects
+ * @author Douglas J Dougan djdougan@gmail.com
+ * @summary Competency 100D exercise at https://www.evolveu.ca/
+ * Created at     : 2020-03-15 18:00:00
+ * Last modified  : 2020-03-16 15:26:51
+ *
+ * @name Account
+ * Creates a stack like with array as it's container
+ * @class
+ */
+class Account {
+    /**
+* @description 
+* @name constructor
+* @param {string} accountName -- name of the account.
+* @param {number} initialBalance -- amount deposited when account was created.
+*/
+    constructor(accountName, initialBalance) {
+        this._accountName = accountName;
+        this._balance = initialBalance;
+    };
+    /**
+  * @description deposits amount to account
+  * @name deposit
+  * @param {number} amount -- amount to be deposit
+ */
+    deposit(amount) {
+        if (amount > 0) {
+            this._balance += amount;
+        }
+    };
+    /**
+  * @description withdraws amount from account
+  * @name withdraw
+  * @param {number} amount -- amount to withdraw from account.
+ */
+    withdraw(amount) {
+        // over draft is allowed
+        this._balance -= amount;
+    };
+    /**
+  * @description gets account balance
+  * @name balance
+  * @return {number} -- return a account balance.
+ */
+    balance() {
+        return this._balance;
+    };
+
+}
+
+export default Account;
---
+++ 03-objects/src/scripts/Account.test.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2020
+ *
+ * @summary Working with arrays project
+ * @author Douglas J Dougan djdougan@gmail.com
+ * @summary Competency 100D exercise at https://www.evolveu.ca/
+ * Created at     : 2020-03-15 18:00:00
+ * Last modified  : 2020-03-16 15:27:19
+ *
+ */
+import Account from './Account'
+
+
+let checking;
+test("Test: Account constructor()", () => {
+    checking = new Account('checkingAccount', 25.00);
+    expect(checking._accountName).toBe("checkingAccount");
+    expect(checking.balance()).toBe(25.00);
+});
+
+test("Test: Account balance()", () => {
+    expect(checking.balance()).toBe(25.00);
+});
+
+test("Test: Account deposit()", () => {
+    checking.deposit(15.50);
+    expect(checking.balance()).toBe(40.50);
+});
+
+test("Test: Account withdraw()", () => {
+    checking.withdraw(15.50);
+    expect(checking.balance()).toBe(25.00);
+});
