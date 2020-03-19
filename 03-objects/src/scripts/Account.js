6b5fdf3a7f6053761a9ae0de11a4680af5ba60d4
Douglas Dougan <djdougan@ddougan.com>
2020-03-19 12:04:08 -0600

    Account controller completed

--- 03-objects/src/scripts/Account.js
+++ 03-objects/src/scripts/Account.js
@@ -5,7 +5,7 @@
  * @author Douglas J Dougan djdougan@gmail.com
  * @summary Competency 100D exercise at https://www.evolveu.ca/
  * Created at     : 2020-03-15 18:00:00
- * Last modified  : 2020-03-17 16:19:09
+ * Last modified  : 2020-03-19 12:01:04
  *
  * @name Account
  * @class
@@ -14,14 +14,14 @@ class Account {
     /**
 * @description 
 * @name constructor
-* @param {string} accountName -- name of the account.
+* @param {string} accName -- name of the account.
 * @param {number} initialBalance -- amount deposited when account was created.
 */
-    constructor(accountName, initialBalance) {
+    constructor(accName, initialBalance) {
         try {
             if (this.isValidNumber(initialBalance)) {
-                this._accountName = accountName;
-                this._balance = initialBalance;
+                this.accountName = accName;
+                this.bal = initialBalance;
             } else {
 
                 throw new Error(`The value ${initialBalance} is not a valid number.`)
@@ -29,7 +29,6 @@ class Account {
         } catch (err) {
             throw err
         }
-
     };
 
     /**
@@ -41,7 +40,7 @@ class Account {
         try {
             if (this.isValidNumber(amount)) {
                 if (amount > 0) {
-                    this._balance += amount;
+                    this.bal += amount;
                 }
             } else {
 
@@ -60,7 +59,7 @@ class Account {
         try {
             if (this.isValidNumber(amount)) {
                 // over draft is allowed
-                this._balance -= amount;
+                this.bal -= amount;
             } else {
 
                 throw new Error(`The value ${amount} is not a valid number.`)
@@ -75,7 +74,7 @@ class Account {
   * @return {number} -- return a account balance.
  */
     balance() {
-        return this._balance;
+        return this.bal;
     };
 
     isValidNumber(number) {
@@ -111,26 +110,106 @@ class AccountController {
     constructor() {
         this.accounts = [];
     }
-    createAccount(account) {
-        if (account) {
-            this.accounts.push(account);
+    createAccount(name, initialBalance) {
+        let result;
+        try {
+            let account = new Account(name, initialBalance);
+            if (account) {
+                this.accounts.push(account);
+                let index = this.accounts.indexOf(this.accounts.find(x => x.accountName === account.accountName));
+                if (this.accounts[index]) {
+                    result = this.accounts[index];
+                } else {
+                    throw "account not created";
+                }
+            }
+        } catch (err) {
+            throw err.message;
         }
+        return result;
     }
-    removeAccount(account) {
+    removeAccount(accName) {
+        let results;
+        try {
+            let acc = this.accounts.find(x => x.accountName === accName);
+            let index = this.accounts.indexOf(acc);
+            if (this.accounts[index]) {
+                this.accounts.splice(index, 1);
+                results = this.accounts[index];
+            } else {
+                throw new Error("account not found");
+            }
+        } catch (err) {
+            throw err.message;
+        }
+        return results;
 
     }
-    nameAccount() {
 
+    nameAccount(prevAccountName, newAccountName) {
+        let result;
+        try {
+            let index = this.accounts.indexOf(this.accounts.find(x => x.accountName === prevAccountName));
+            if (this.accounts[index]) {
+                this.accounts[index].accountName = newAccountName;
+                result = this.accounts[index];
+            } else {
+                throw new Error("account not found");
+            }
+        } catch (err) {
+            throw err.message;
+        }
+
+        return result;
     }
 
-    getAccountTotal(account) {
-
+    getAccountTotal(accountName) {
+        let result;
+        try {
+            let index = this.accounts.indexOf(this.accounts.find(x => x.accountName === accountName));
+            if (this.accounts[index]) {
+                result = this.accounts[index].bal;
+            } else {
+                throw new Error(`${accountName} not found`)
+            }
+        } catch (err) {
+            throw err.message;
+        }
+        return result;
     }
+
     getHighestValuedAccount() {
-
+        let result;
+        try {
+            if (this.accounts) {
+                this.accounts.sort(function (a, b) {
+                    return b.bal - a.bal
+                });
+                result = this.accounts[0];
+            } else {
+                throw new Error(`accounts not found`)
+            }
+        } catch (err) {
+            throw err.message;
+        }
+        return result;
     }
     getLowestValuedAccount() {
 
+        let result;
+        try {
+            if (this.accounts) {
+                this.accounts.sort(function (a, b) {
+                    return a.bal - b.bal
+                });
+                result = this.accounts[0];
+            } else {
+                throw new Error(`accounts not found`)
+            }
+        } catch (err) {
+            throw err.message;
+        }
+        return result;
     }
 
 
