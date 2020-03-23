30c01576e9ab6e99775d1d0ec0bbc99564f2c31e
Douglas Dougan <djdougan@ddougan.com>
2020-03-22 18:08:57 -0600

    fixed bug initial deposit was a string

--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -20,7 +20,7 @@ let activeAccount = "";
 btnCreateAccount.addEventListener("click", (e) => {
     // debugger;
     let accountName = txtAccountName.value;
-    let balance = txtStartingBalance.value;
+    let balance = parseInt(txtStartingBalance.value);
     let account = AC.createAccount(accountName, balance, MiscScripts.createUUID());
     let card = new Card(account);
     let div = card.buildCard();
@@ -41,7 +41,6 @@ function cardClick(e) {
 function closeAccount(e) {
 
     let card = e.target.parentElement;
-    console.log(card);
     e.preventDefault();
     e.stopPropagation();
     if (confirm('Are you sure you want to close this Account?')) {
