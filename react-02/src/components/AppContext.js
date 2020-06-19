ff047bea99b84e00e110d8767ad6e7a94bebc30b
Douglas Dougan <djdougan@ddougan.com>
2020-06-19 13:07:29 -0600

    add AppContext

---
+++ react-02/src/components/AppContext.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import App from "../App";
+
+import DoublyLinkedList from "../BLL/Data-Structures/DoublyLinkedList.js";
+
+export const AppContext = React.createContext();
+
+export class ContextProvider extends Component {
+  dLinkedList = new DoublyLinkedList();
+
+  state = {
+    current: null,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleStateChange = (state) => {
+    for (let i = 0; i < state.length; i++) {
+      this.setState({
+        [state[i].state]: state[i].newState,
+      });
+    }
+  };
+  render() {
+    return (
+      <AppContext.Provider
+        value={{
+          dLinkedList: this.dLinkedList,
+        }}>
+        <App />
+      </AppContext.Provider>
+    );
+  }
+}
