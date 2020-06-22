7254497fe7640292b5d68fe15c52b2d4a8c280ec
Douglas Dougan <djdougan@ddougan.com>
2020-06-22 07:49:22 -0600

    merge repository

--- react-02/src/components/Account/AccountApp.js
+++ react-02/src/components/Account/AccountApp.js
@@ -1,10 +1,6 @@
 import { AppContext } from "../../components/AppContext";
-<<<<<<< HEAD
-import React, { Component, useContext } from "react";
-=======
 
 import React, { Component } from "react";
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
 
 import AccountList from "./AccountList";
 import AccountControl from "./AccountControl";
@@ -13,73 +9,6 @@ import AccountControl from "./AccountControl";
 class AccountApp extends Component {
   constructor() {
     super();
-<<<<<<< HEAD
-
-    // context = {
-    //   accountList: [],
-    //   total: 0,
-    //   highestAccount: { accountName: "", balance: 0, key: null },
-    //   lowestAccount: { accountName: "", balance: 0, key: null },
-    //   newAccount: { accountName: "", balance: 0, key: null },
-    // };
-    // this.accountCtrl = new AccountController();
-    this.createAccount = this.createAccount.bind(this);
-  }
-
-  static contextType = AppContext;
-  componentWillMount() {
-    if (this.context.accountCtrl.accounts.length >= 1) {
-      let highest = this.context.accountCtrl.getHighestValuedAccount();
-      let lowest = this.context.accountCtrl.getLowestValuedAccount();
-
-      this.context.handleStateChange({
-        accountList: this.context.accountCtrl.accounts,
-        total: this.context.accountCtrl.getAccountTotal,
-        highestAccount: {
-          accountName: highest.accountName,
-          balance: highest.balance,
-          key: highest.key,
-        },
-        lowestAccount: {
-          accountName: lowest.accountName,
-          balance: lowest.balance,
-          key: lowest.key,
-        },
-      });
-    }
-  }
-  createAccount = (e) => {
-    this.context.accountCtrl.createAccount(
-      this.context.newAccount.accountName,
-      this.context.newAccount.balance
-    );
-    this.context.handleStateChange({ accountList: this.context.accounts });
-    // this.setState({
-    //   accountList: this.accountCtrl.accounts,
-    // });
-  };
-
-  // handleChange=(e)=> {
-  //     const { newAccount } = { ...context };
-  //     console.log(newAccount)
-  //     const account = newAccount;
-  //     const { name, value } = e.target;
-  //     account[name] = value;
-  //     this.setState({ newAccount: {
-  //             accountName: newAccount.accountName,
-  //             balance: newAccount.balance,
-  //             key: newAccount.key
-  //         }});
-
-  // }
-
-  handleDelete = (key) => {
-    this.accountCtrl.removeAccount(key);
-    this.context.handleStateChange({ accountList: this.context.accounts });
-    // this.setState({
-    //   AccountList: this.accountCtrl.accounts,
-    // });
-=======
     this.createAccount = this.createAccount.bind(this);
   }
   static contextType = AppContext;
@@ -106,7 +35,6 @@ class AccountApp extends Component {
     this.context.handleLowestAccountState(
       this.context.accountCtrl.getLowestValuedAccount()
     );
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
   };
 
   render() {
@@ -117,28 +45,11 @@ class AccountApp extends Component {
           <AccountControl
             onCreate={this.createAccount}
             onChange={this.handleChange}
-<<<<<<< HEAD
-            newAccount={this.context.newAccount}
-=======
             newAccount={this.context.state.newAccount}
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
           />
         </div>
         <div>
           <h2>
-<<<<<<< HEAD
-            Accounts total: <span>{this.context.total}</span>
-          </h2>
-          <h2>
-            Largest account:{" "}
-            <span>{this.context.highestAccount.accountName}</span>
-            <span>{this.context.highestAccount.balance}</span>
-          </h2>
-          <h2>
-            Smallest account:{" "}
-            <span>{this.context.lowestAccount.accountName}</span>
-            <span>{this.context.lowestAccount.balance}</span>
-=======
             Accounts total: <span>{this.context.state.total}</span>
           </h2>
           <h2>
@@ -150,16 +61,11 @@ class AccountApp extends Component {
             Smallest account:
             <span>{this.context.state.lowestAccount.accountName}</span>
             <span>{this.context.state.lowestAccount.balance}</span>
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
           </h2>
         </div>
         <div>
           <AccountList
-<<<<<<< HEAD
-            accountList={this.context.accountList}
-=======
             accounts={this.context.state.accounts}
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
             handleDelete={this.handleDelete}
           />
         </div>
--- react-02/src/components/AppContext.js
+++ react-02/src/components/AppContext.js
@@ -2,40 +2,17 @@ import React, { Component } from "react";
 import App from "../App";
 
 import DoublyLinkedList from "../BLL/Data-Structures/DoublyLinkedList.js";
-<<<<<<< HEAD
-import Queue from "../BLL/Data-Structures/Queue.js";
-import Stack from "../BLL/Data-Structures/Stack.js";
-import { AccountController } from "../BLL/Account/Account.js";
-=======
 import { AccountController } from "../BLL/Account/Account.js";
 
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
 export const AppContext = React.createContext();
 
 export class ContextProvider extends Component {
   dLinkedList = new DoublyLinkedList();
-<<<<<<< HEAD
-  queue = new Queue();
-  stack = new Stack();
-=======
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
   accountCtrl = new AccountController();
 
   state = {
     current: null,
-<<<<<<< HEAD
-    queue: [],
-    stack: [],
-    history: [
-      {
-        squares: Array(9).fill(null),
-      },
-    ],
-
-    accountList: [],
-=======
     accounts: [],
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
     total: 0,
     highestAccount: { accountName: "", balance: 0, key: null },
     lowestAccount: { accountName: "", balance: 0, key: null },
@@ -55,9 +32,6 @@ export class ContextProvider extends Component {
       });
     }
   };
-<<<<<<< HEAD
-
-=======
   handleAccountState = (accounts) => {
     console.log(accounts);
     this.setState({ accounts: accounts });
@@ -70,32 +44,17 @@ export class ContextProvider extends Component {
     console.log(accounts);
     this.setState({ lowestAccount: accounts });
   };
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
   render() {
     return (
       <AppContext.Provider
         value={{
           dLinkedList: this.dLinkedList,
-<<<<<<< HEAD
-          queue: this.queue,
-          stack: this.stack,
-          accountCtrl: this.accountCtrl,
-          accountList: this.accountList,
-          total: this.total,
-          highestAccount: this.highestAccount,
-          lowestAccount: this.lowestAccount,
-          newAccount: this.newAccount,
-
-          handleStateChange: this.handleStateChange,
-          handleChange: this.handleChange,
-=======
           accountCtrl: this.accountCtrl,
           handleStateChange: this.handleStateChange,
           handleAccountState: this.handleAccountState,
           handleHighestAccountState: this.handleHighestAccountState,
           handleLowestAccountState: this.handleLowestAccountState,
           state: this.state,
->>>>>>> a27a3730a14baf9cc729a3aecc4fc64a60118714
         }}>
         <App />
       </AppContext.Provider>
